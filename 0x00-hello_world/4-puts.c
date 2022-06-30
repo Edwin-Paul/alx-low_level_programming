a19ab40f9a4fc734096dc45c65cca09c0b09bc43
Edwin-Paul <github.paul01@gmail.com>
2022-06-30 14:40:51 -0700

    Printing in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
