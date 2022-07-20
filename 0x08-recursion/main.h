9a04c9ff67c971da71e2f1535cb825455b368e8e
Edwin-Paul <github.paul01@gmail.com>
2022-07-20 12:03:00 -0700

    prints a string and a new line

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: Main.h
+ *
+ *description: Header file containing prototypes for all functions
+ *
+ */
+
+int _putchar(char c);
+void_puts_recursion(char *s);
